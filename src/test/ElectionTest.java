4c029e0b7386e261057fd8381fd5d2f03e7f78f5
luizademelo <ludemelo5452@gmail.com>
2024-06-26 20:07:25 -0300

    adição de mais 3 testes de integração

--- src/test/ElectionTest.java
+++ src/test/ElectionTest.java
@@ -11,9 +11,67 @@ import com.election.entity.UVoter;
 import com.election.entity.Vote;
 import com.election.entity.Voter;
 import com.election.view.ReadAndPrint;
+import com.election.view.ReadAndPrintMunicipal;
+import com.election.view.ReadAndPrintPresidential;
+import com.election.view.ReadAndPrintState;
 import com.election.view.ReadAndPrintUDepartment;
 
 public class ElectionTest {
+
+    @Test
+    public void testPresidentialElection() {
+        ElectionController.createElection("password", "presidencial");
+        ReadAndPrint.loadVotersAndProfessionals();
+        ElectionController.currentElection.setRound("FIRST_ROUND");
+        ReadAndPrintPresidential.loadCandidates();
+        ElectionController.currentElection.setStatus("RUNNING");
+        Candidate candidate1 = ReadAndPrint.CandidateMap.get(13);
+        Vote vote1 = new Vote("valid", candidate1);
+        ElectionController.voteList.add(vote1);
+        Candidate candidate2 = ReadAndPrint.CandidateMap.get(1212);
+        Vote vote2 = new Vote("valid", candidate2);
+        ElectionController.voteList.add(vote2);
+        ElectionController.finishElection();
+        List<Candidate> winners = ElectionController.getResults();
+        Candidate c1 = winners.get(0);
+        Candidate c2 = winners.get(1);
+        assertEquals(c1.getName(), "Lula");
+        assertEquals(c2.getName(), "Duda Salabert");
+
+    }
+
+    @Test
+    public void testMunicipalElection() {
+        ElectionController.createElection("password", "municipal");
+        ReadAndPrint.loadVotersAndProfessionals();
+        ElectionController.currentElection.setRound("FIRST_ROUND");
+        ReadAndPrintMunicipal.loadCandidates();
+        ElectionController.currentElection.setStatus("RUNNING");
+        Candidate candidate = ReadAndPrint.CandidateMap.get(35);
+        Vote vote = new Vote("valid", candidate);
+        ElectionController.voteList.add(vote);
+        ElectionController.finishElection();
+        List<Candidate> winners = ElectionController.getResults();
+        Candidate c = winners.get(0);
+        assertEquals(c.getName(), "Edmar");
+    }
+
+    @Test
+    public void testStateElection() {
+        ElectionController.createElection("password", "estadual");
+        ReadAndPrint.loadVotersAndProfessionals();
+        ElectionController.currentElection.setRound("FIRST_ROUND");
+        ReadAndPrintState.loadCandidates();
+        ElectionController.currentElection.setStatus("RUNNING");
+        Candidate candidate = ReadAndPrint.CandidateMap.get(15456);
+        Vote vote = new Vote("valid", candidate);
+        ElectionController.voteList.add(vote);
+        ElectionController.finishElection();
+        List<Candidate> winners = ElectionController.getResults();
+        Candidate c = winners.get(0);
+        assertEquals(c.getName(), "Ivo");
+    }
+
     @Test
     public void testUdepartmentElection() {
         ElectionController.createElection("password", "universitaria");
